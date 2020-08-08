59b905b18aa6ee61fa57c8aa3726425837beede9
leduclinh-it <leduclinh65@gmail.com>
2020-08-08 23:33:26 +0700

    init

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+@NgModule({
+  declarations: [],
+  imports: [
+    MDBBootstrapModule.forRoot()
+  ],
+  exports: [
+    MDBBootstrapModule
+  ]
+})
+export class SharedModule {}
