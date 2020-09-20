1dae0ab0e1c0f49b4b8e0e9b623958a9ac8e5046
leduclinh-it <leduclinh65@gmail.com>
2020-09-20 23:19:38 +0700

    init

---
+++ cd-busines-core/src/main/java/dev/leduclinh/cdbusiness/CdBusinessApplication.java
@@ -0,0 +1,13 @@
+package dev.leduclinh.cdbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdBusinessApplication.class, args);
+    }
+
+}
